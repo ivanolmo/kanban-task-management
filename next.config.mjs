67d9079b8b36ab0dfacac13a65333babd24f66be
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-14 20:37:09 -0500

    Initial commit for task management app

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import { env } from './src/env/server.mjs';
+
+/**
+ * @template {import('next').NextConfig} T
+ * @param {T} config
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config) {
+  return config;
+}
+
+export default defineNextConfig({
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+});
