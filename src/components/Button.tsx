c8c39be75284e0ffc81d5affdb4c9416bd0065e7
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-15 22:44:36 -0500

    Add reusable button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,31 @@
+interface Props {
+  children: React.ReactNode;
+  onClick?: () => void;
+  variant?: string;
+  size?: string;
+  disabled?: boolean;
+}
+
+const Button: React.FC<Props> = ({
+  children,
+  onClick,
+  variant = 'primary',
+  size = 'rg',
+  disabled,
+  ...rest
+}) => {
+  return (
+    <button
+      className={`flex justify-center items-center gap-1 btn ${variant} ${size} ${
+        disabled ? 'disabled' : ''
+      }`}
+      disabled={disabled}
+      onClick={onClick}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
