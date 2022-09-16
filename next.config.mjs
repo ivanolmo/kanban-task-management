e40c651c28327f34a5ab60c5cd54d32969311108
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-16 18:59:04 -0500

    Add svgr package and update next config

--- next.config.mjs
+++ next.config.mjs
@@ -16,4 +16,12 @@ export default defineNextConfig({
     locales: ['en'],
     defaultLocale: 'en',
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 });
