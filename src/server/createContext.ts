8b7ceca8cd15354aca99a1c35f630cc58b4d667d
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-17 22:36:45 -0500

    Add trpc context/router creator files

--- src/server/createContext.ts
+++ src/server/createContext.ts
@@ -1,9 +1,8 @@
-// src/server/router/context.ts
-import * as trpc from "@trpc/server";
-import * as trpcNext from "@trpc/server/adapters/next";
-import { Session } from "next-auth";
-import { getServerAuthSession } from "../../server/common/get-server-auth-session";
-import { prisma } from "../db/client";
+import * as trpc from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
+import { Session } from 'next-auth';
+import { getServerAuthSession } from './common/get-server-auth-session';
+import { prisma } from './db/client';
 
 type CreateContextOptions = {
   session: Session | null;
@@ -25,7 +24,7 @@ export const createContextInner = async (opts: CreateContextOptions) => {
  * @link https://trpc.io/docs/context
  **/
 export const createContext = async (
-  opts: trpcNext.CreateNextContextOptions,
+  opts: trpcNext.CreateNextContextOptions
 ) => {
   const { req, res } = opts;
 
@@ -37,24 +36,22 @@ export const createContext = async (
   });
 };
 
-type Context = trpc.inferAsyncReturnType<typeof createContext>;
-
-export const createRouter = () => trpc.router<Context>();
+export type Context = trpc.inferAsyncReturnType<typeof createContext>;
 
 /**
  * Creates a tRPC router that asserts all queries and mutations are from an authorized user. Will throw an unauthorized error if a user is not signed in.
  **/
-export function createProtectedRouter() {
-  return createRouter().middleware(({ ctx, next }) => {
-    if (!ctx.session || !ctx.session.user) {
-      throw new trpc.TRPCError({ code: "UNAUTHORIZED" });
-    }
-    return next({
-      ctx: {
-        ...ctx,
-        // infers that `session` is non-nullable to downstream resolvers
-        session: { ...ctx.session, user: ctx.session.user },
-      },
-    });
-  });
-}
+// export function createProtectedRouter() {
+//   return createRouter().middleware(({ ctx, next }) => {
+//     if (!ctx.session || !ctx.session.user) {
+//       throw new trpc.TRPCError({ code: 'UNAUTHORIZED' });
+//     }
+//     return next({
+//       ctx: {
+//         ...ctx,
+//         // infers that `session` is non-nullable to downstream resolvers
+//         session: { ...ctx.session, user: ctx.session.user },
+//       },
+//     });
+//   });
+// }
