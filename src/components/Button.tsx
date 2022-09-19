33e3edf0047c7f48c2b74cbf9571bace14e05774
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-18 23:34:44 -0500

    Update button to take additional props
    Added type and hidden

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,29 @@
 interface Props {
   children: React.ReactNode;
   onClick?: () => void;
+  type?: 'button' | 'submit';
   variant?: string;
   size?: string;
   disabled?: boolean;
+  hidden?: boolean;
 }
 
 const Button: React.FC<Props> = ({
   children,
   onClick,
+  type = 'button',
   variant = 'primary',
   size = 'rg',
   disabled,
+  hidden,
   ...rest
 }) => {
   return (
     <button
+      type={type}
       className={`flex justify-center items-center gap-1 btn group text-md ${variant} ${size} ${
         disabled ? 'disabled' : ''
-      }`}
+      } ${hidden ? 'hidden' : ''}`}
       disabled={disabled}
       onClick={onClick}
       {...rest}
