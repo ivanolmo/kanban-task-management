975721c021bbb62f65f69bc5e242547e684ea36d
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-22 13:40:23 -0500

    Clean up options in next-auth api route

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,23 +3,11 @@ import DiscordProvider from 'next-auth/providers/discord';
 import EmailProvider from 'next-auth/providers/email';
 import GithubProvider from 'next-auth/providers/github';
 
-// Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { prisma } from '@/server/db/client';
-
 import { env } from '../../../env/server.mjs';
 
 export const authOptions: NextAuthOptions = {
-  secret: env.NEXTAUTH_SECRET,
-  callbacks: {
-    async session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
-  // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
@@ -41,13 +29,14 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
   ],
-  session: {
-    maxAge: 30 * 24 * 60 * 60, // 30 days
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
   },
-  jwt: {
-    secret: env.NEXTAUTH_SECRET,
-  },
-  debug: true,
 };
 
 export default NextAuth(authOptions);
