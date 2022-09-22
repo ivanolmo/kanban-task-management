5be9c2c143c75c2e84f902e78586f5d17558109e
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-22 12:46:29 -0500

    Add auth router to main app router

--- src/server/router/app.router.ts
+++ src/server/router/app.router.ts
@@ -1,9 +1,9 @@
 import { createRouter } from '../createRouter';
-import userRouter from './user.router';
 import boardRouter from './board.router';
+import authRouter from './auth.router';
 
 const appRouter = createRouter()
-  .merge('users.', userRouter)
+  .merge('auth.', authRouter)
   .merge('boards.', boardRouter);
 
 export type AppRouter = typeof appRouter;
