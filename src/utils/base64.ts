b64fdebc449cde13f4742d2f3048cb287398cb0b
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-22 13:02:34 -0500

    Clean up deleted files and uninstalled deps

--- src/utils/base64.ts
+++
@@ -1,9 +0,0 @@
-const encode = (data: string) => {
-  return Buffer.from(data, 'utf-8').toString('base64');
-};
-
-const decode = (data: string) => {
-  return Buffer.from(data, 'base64').toString('utf-8');
-};
-
-export { encode, decode };
--- src/utils/jwt.ts
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const SECRET = process.env.JWT_SECRET || 'changeme';
-
-const signJwt = (data: object) => jwt.sign(data, SECRET);
-const verifyJwt = <T>(token: string) => jwt.verify(token, SECRET) as T;
-
-export { signJwt, verifyJwt };
