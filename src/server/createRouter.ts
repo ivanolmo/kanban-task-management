38f164922e8cbd3d5a680c7ae8b74f83ef74e5fd
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-24 17:17:50 -0500

    Remove deleted trpc server files

--- src/server/createRouter.ts
+++
@@ -1,8 +0,0 @@
-import { router } from '@trpc/server';
-import superjson from 'superjson';
-
-import { Context } from './createContext';
-
-export const createRouter = () => {
-  return router<Context>().transformer(superjson);
-};
--- src/server/router/app.router.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter } from '../createRouter';
-import authRouter from './auth.router';
-import boardRouter from './board.router';
-
-export const appRouter = createRouter()
-  .merge('auth.', authRouter)
-  .merge('boards.', boardRouter);
-
-export type AppRouter = typeof appRouter;
