be7f66904c60fe253f977b52ea1e80f5e6953b9c
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-25 23:11:46 -0500

    Add type and board name to Column component

--- src/components/Column/ColumnComp.tsx
+++ src/components/Column/ColumnComp.tsx
@@ -1,14 +1,19 @@
 import ColumnItem from './ColumnItem';
+import type { Column } from 'src/types/boardTypes';
 
-const ColumnComp = () => {
+type ColumnProps = {
+  column: Column;
+};
+
+const ColumnComp = (props: ColumnProps) => {
   return (
     <div className='flex flex-col gap-6 w-72 flex-shrink-0'>
       <div className='flex items-center gap-3'>
         {/* color of circle customizable? */}
         <span className='w-4 h-4 bg-red-600 rounded-full flex-shrink-0'></span>
-        <h4 className='text-slate uppercase'>Todo (4)</h4>
+        <h4 className='text-slate uppercase'>{props.column?.columnName}</h4>
       </div>
-      {/* task array map */}
+      {/* implement task array map */}
       <ul className='space-y-5'>
         <ColumnItem />
         <ColumnItem />
