fe350e0b88738cc1be435e5a419290e5ae45629a
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-25 23:27:06 -0500

    Refactor SidebarItem comp to function as intended
    Added prop types
    Bg color conditional on selected board
    Clicking item will change selected board
    Shows board name

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -1,13 +1,26 @@
 import BoardIcon from '@/assets/icon-board.svg';
+import type { Board } from 'src/types/boardTypes';
 
-// This comp will be used in map of existing board list to display boards in sidebar
-// conditional bg/text color based on active board
-const SidebarItem = () => {
+type SidebarItemProps = {
+  board: Board;
+  selected: boolean;
+  handleSelectBoard: (board: Board) => void;
+};
+
+const SidebarItem = (props: SidebarItemProps) => {
   return (
-    <li className='py-4 px-6 rounded-r-full mr-4'>
-      <a href='#' className='flex items-center gap-3 text-md font-bold '>
-        <BoardIcon className='fill-slate' />
-        <span className=''>Placeholder title</span>
+    <li
+      className={`py-4 px-6 rounded-r-full mr-4 ${
+        props.selected && 'bg-violet-700 text-white'
+      }`}
+    >
+      <a
+        href='#'
+        className='flex items-center gap-3 text-md font-bold'
+        onClick={() => props.handleSelectBoard(props.board)}
+      >
+        <BoardIcon className='fill-current' />
+        <span className=''>{props.board.boardName}</span>
       </a>
     </li>
   );
