c191b92cea253de08570f5a7d198543130f82df0
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-26 21:54:26 -0500

    Refactor Modal container comp for use on mobile screen
    Now takes a 'mobile' prop that changes styling on mobile screen size

--- src/components/Modal/ModalContainer.tsx
+++ src/components/Modal/ModalContainer.tsx
@@ -1,9 +1,24 @@
-export const Modal = ({ children }: { children: React.ReactNode }) => {
+type ModalProps = {
+  children: React.ReactNode;
+  mobile?: boolean;
+};
+
+export const Modal = (props: ModalProps) => {
   return (
     <>
-      <div className='absolute inset-0 bg-gradient' />
-      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center gap-0 bg-white p-8 rounded-lg w-11/12 md:w-9/12 max-w-xl'>
-        {children}
+      <div
+        className={`absolute inset-x-0 bg-gradient ${
+          props.mobile ? 'top-16 bottom-0' : 'inset-y-0'
+        }`}
+      />
+      <div
+        className={`absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center gap-0 bg-white rounded-lg ${
+          props.mobile
+            ? 'top-1/3 p-0 w-9/12 max-w-sm'
+            : 'top-1/2 p-8 w-11/12 md:w-9/12 max-w-xl'
+        }`}
+      >
+        {props.children}
       </div>
     </>
   );
