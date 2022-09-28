9f5ee74e3df4d770a42ac9c1900c8b7ddcfddacb
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-27 23:49:49 -0500

    Merge new column route in main app router

--- src/server/router/app.ts
+++ src/server/router/app.ts
@@ -1,6 +1,9 @@
 import { createRouter } from '../context';
 import { boardRouter } from './board';
+import { columnRouter } from './column';
 
-export const appRouter = createRouter().merge('boards.', boardRouter);
+export const appRouter = createRouter()
+  .merge('boards.', boardRouter)
+  .merge('columns.', columnRouter);
 
 export type AppRouter = typeof appRouter;
