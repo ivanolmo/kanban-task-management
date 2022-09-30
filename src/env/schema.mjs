2e4d7291bdfaef8d0d3d66d8bc53142580978386
Ivan Olmo <ivan.olmo@gmail.com>
2022-09-29 21:54:53 -0500

    Fix env variable config

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -9,7 +9,6 @@ export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(['development', 'test', 'production']),
   NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL: z.string().url(),
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
   DISCORD_CLIENT_ID: z.string(),
