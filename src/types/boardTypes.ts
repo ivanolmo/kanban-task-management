f689755d48d6338b6ee307a3c9c9a8dce828d086
Ivan Olmo <ivan.olmo@gmail.com>
2022-10-04 19:47:57 -0500

    Add two new types -> Task and Subtask

--- src/types/boardTypes.ts
+++ src/types/boardTypes.ts
@@ -2,6 +2,27 @@ export type Column = {
   id: string;
   boardId: string;
   columnName: string;
+  tasks: Task[];
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type Task = {
+  id: string;
+  columnId: string;
+  title: string;
+  description: string;
+  completed: boolean;
+  subtasks: Subtask[];
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type Subtask = {
+  id: string;
+  taskId: string;
+  title: string;
+  completed: boolean;
   createdAt: Date;
   updatedAt: Date;
 };
