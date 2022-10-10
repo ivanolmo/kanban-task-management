f323dbe2364176e7cdc4d6725eff8042ee789c42
Ivan Olmo <ivan.olmo@gmail.com>
2022-10-09 22:15:35 -0500

    Add simple error handling to trpc api route

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -6,4 +6,12 @@ import { createContext } from '@/server/context';
 export default createNextApiHandler({
   router: appRouter,
   createContext: createContext,
+  onError({ error }) {
+    console.error('tRPC error -> ', error);
+    if (error instanceof Error) {
+      return {
+        message: error.message,
+      };
+    }
+  },
 });
