9f125c4366231241d0efeccc8e339ede3d62b419
Ivan Olmo <ivan.olmo@gmail.com>
2022-10-10 01:23:48 -0500

    Fix error introduced in previous commit
    Changed session from type back to interface

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,9 +4,9 @@ declare module 'next-auth' {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
-  type Session = {
+  interface Session {
     user?: {
       id: string;
     } & DefaultSession['user'];
-  };
+  }
 }
