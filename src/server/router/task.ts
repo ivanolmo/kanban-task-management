dd433841adfbebdc7fb6d92b867f7012f102ae8d
Ivan Olmo <ivan.olmo@gmail.com>
2022-10-12 21:11:42 -0500

    Update task router with delete-task mutation

--- src/server/router/task.ts
+++ src/server/router/task.ts
@@ -11,24 +11,37 @@ const createTaskSchema = z.object({
 
 export type CreateTaskInput = z.TypeOf<typeof createTaskSchema>;
 
-export const taskRouter = createProtectedRouter().mutation('add-task', {
-  input: createTaskSchema,
-  resolve: async ({ ctx, input }) => {
-    const task = await ctx.prisma.task.create({
-      data: {
-        columnId: input.columnId.id,
-        title: input.title,
-        description: input.description,
-        subtasks: {
-          createMany: {
-            data: input?.subtasks?.map((subtask) => ({
-              title: subtask.title,
-            })),
+export const taskRouter = createProtectedRouter()
+  .mutation('add-task', {
+    input: createTaskSchema,
+    resolve: async ({ ctx, input }) => {
+      const task = await ctx.prisma.task.create({
+        data: {
+          columnId: input.columnId.id,
+          title: input.title,
+          description: input.description,
+          subtasks: {
+            createMany: {
+              data: input?.subtasks?.map((subtask) => ({
+                title: subtask.title,
+              })),
+            },
           },
         },
-      },
-    });
+      });
 
-    return task;
-  },
-});
+      return task;
+    },
+  })
+  .mutation('delete-task', {
+    input: z.string(),
+    resolve: async ({ ctx, input }) => {
+      const task = await ctx.prisma.task.delete({
+        where: {
+          id: input,
+        },
+      });
+
+      return task;
+    },
+  });
