842671919942d70063578ed4505058af29f6caf1
Ivan Olmo <ivan.olmo@gmail.com>
2022-10-21 23:40:35 -0500

    Refactor comp to display column color received from db

--- src/components/Column/ColumnComp.tsx
+++ src/components/Column/ColumnComp.tsx
@@ -7,21 +7,12 @@ type ColumnProps = {
 };
 
 const ColumnComp = (props: ColumnProps): JSX.Element => {
-  enum Colors {
-    todo = '#49c4e5',
-    doing = '#8471f2',
-    done = '#67e2ae',
-  }
-
-  const color =
-    Colors[props.column?.columnName.toLowerCase() as keyof typeof Colors];
-
   return (
     <div className='flex flex-col gap-6 w-72 flex-shrink-0'>
       <div className='flex items-center gap-3 text-slate uppercase'>
         <span
           className='w-4 h-4 rounded-full flex-shrink-0'
-          style={{ backgroundColor: color }}
+          style={{ backgroundColor: `#${props.column.accentColor}` }}
         ></span>
         <h4>{props.column?.columnName}</h4>
         <span className='text-body-md tracking-widest -ml-2'>{`(${props.column?.tasks?.length})`}</span>
